09363547cbfca45c3bae7a0acc781f89b0baaeeb
Vishal singh <vishalrajput862@gmail.com>
2017-07-04 15:29:10 +0530

    Add files via upload

---
+++ oop/arr_duplicate.php
@@ -0,0 +1,23 @@
+<?php
+ //given array function finds the duplicate number 
+ $arr = array(3,5,2,5,3,9,4,6,4);
+  $temp_array = array();
+
+  foreach($arr as $val)
+  {
+     if(isset($temp_array[$val]))
+     {
+       $temp_array[$val] = $val;
+    }else{
+     $temp_array[$val] = 0;
+   }
+ }
+
+ foreach($temp_array as $val2)
+ {
+    if($val2 > 0)
+    {
+      echo $val2 . ', ';
+    }
+ }
+ ?>
